8e39d5b1f29be45d7c1fac42abd92b25405b62d5
John Lee <ljonghan@gmail.com>
2022-06-23 12:30:01 -0400

    added truthtable and outputs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        boolean[][] logicTable = {{true,true},{true,false},{false,false},{false,true}};
+        
+        System.out.println("Truth table for && operator");
+        for(boolean[] boolPair : logicTable){
+            System.out.println(boolPair[0] + " && " + boolPair[1] + " = " + (boolPair[0] && boolPair[1]));
+        }
+
+        System.out.println("Truth table for || operator");
+        for(boolean[] boolPair : logicTable){
+            System.out.println(boolPair[0] + " || " + boolPair[1] + " = " + (boolPair[0] || boolPair[1]));
+        }
     }
 }
